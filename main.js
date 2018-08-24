10863dffbaab619bfbb3f13bbbbcdb1aa9e6bfcd
ENT8R <info.ent8r@gmail.com>
2018-08-24 15:29:11 +0200

    Added Subresource Integrity

--- main.js
+++ main.js
@@ -1,41 +1,41 @@
-var attribution = {
+const attribution = {
   mapzen: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/" target="_blank">Mapzen</a>',
   mapbox: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://mapbox.com/" target="_blank">Mapbox</a>',
   terrestris: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://www.terrestris.de/" target="_blank">Terrestris</a>'
 };
 
-var mapzenLight = Tangram.leafletLayer({
+const mapzenLight = Tangram.leafletLayer({
     scene: 'https://rawgit.com/ENT8R/streetcomplete-mapstyle/master/streetcomplete-light-style.yaml',
     attribution: attribution.mapzen
 });
-var mapzenDark = Tangram.leafletLayer({
+const mapzenDark = Tangram.leafletLayer({
     scene: 'https://rawgit.com/ENT8R/streetcomplete-mapstyle/master/streetcomplete-dark-style.yaml',
     attribution: attribution.mapzen
 });
 
-var mapboxLight = Tangram.leafletLayer({
+const mapboxLight = Tangram.leafletLayer({
     scene: 'https://rawgit.com/ENT8R/streetcomplete-mapstyle/mapbox/streetcomplete-light-style.yaml',
     attribution: attribution.mapbox
 });
-var mapboxDark = Tangram.leafletLayer({
+const mapboxDark = Tangram.leafletLayer({
     scene: 'https://rawgit.com/ENT8R/streetcomplete-mapstyle/mapbox/streetcomplete-dark-style.yaml',
     attribution: attribution.mapbox
 });
-var mapboxSatellite = Tangram.leafletLayer({
+const mapboxSatellite = Tangram.leafletLayer({
     scene: 'https://rawgit.com/ENT8R/streetcomplete-mapstyle/master/streetcomplete-satellite-style.yaml',
     attribution: attribution.mapbox
 });
 
-var terrestrisLight = Tangram.leafletLayer({
+const terrestrisLight = Tangram.leafletLayer({
     scene: 'https://rawgit.com/ENT8R/streetcomplete-mapstyle/terrestris/streetcomplete-light-style.yaml',
     attribution: attribution.terrestris
 });
-var terrestrisDark = Tangram.leafletLayer({
+const terrestrisDark = Tangram.leafletLayer({
     scene: 'https://rawgit.com/ENT8R/streetcomplete-mapstyle/terrestris/streetcomplete-dark-style.yaml',
     attribution: attribution.terrestris
 });
 
-var map = L.map('map', {
+const map = L.map('map', {
     layers: [mapzenLight]
 });
 L.control.layers({
@@ -51,9 +51,9 @@ L.control.layers({
 map.setView([40.70531887544228, -74.00976419448853], 15);
 
 if (typeof(Storage) !== 'undefined') {
-    var lat = localStorage.getItem('lat');
-    var lng = localStorage.getItem('lng');
-    var zoom = localStorage.getItem('zoom');
+    const lat = localStorage.getItem('lat');
+    const lng = localStorage.getItem('lng');
+    const zoom = localStorage.getItem('zoom');
 
     if (lat !== null && lng !== null && zoom !== null) {
       map.setView([lat, lng], zoom);
