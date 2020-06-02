5499995051eb73b023c237b3db77b0fe9c8b03e8
ENT8R <info.ent8r@gmail.com>
2020-06-02 15:54:14 +0200

    Improve the webpage

--- main.js
+++ main.js
@@ -1,69 +1,47 @@
 const attribution = {
-  mapzen: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/" target="_blank">Mapzen</a>',
-  mapbox: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://mapbox.com/" target="_blank">Mapbox</a>',
-  terrestris: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://www.terrestris.de/" target="_blank">Terrestris</a>'
+  mapzen: '<a href="https://mapzen.com/tangram" target="_blank" rel="noopener">Tangram</a> | &copy; OpenStreetMap | <a href="https://map-data.de/" target="_blank" rel="noopener">Map-Data</a>'
 };
 
-const mapzenLight = Tangram.leafletLayer({
-    scene: 'https://cdn.jsdelivr.net/gh/ENT8R/streetcomplete-mapstyle@master/streetcomplete-light-style.yaml',
-    attribution: attribution.mapzen
-});
-const mapzenDark = Tangram.leafletLayer({
-    scene: 'https://cdn.jsdelivr.net/gh/ENT8R/streetcomplete-mapstyle@master/streetcomplete-dark-style.yaml',
-    attribution: attribution.mapzen
-});
-
-const mapboxLight = Tangram.leafletLayer({
-    scene: 'https://cdn.jsdelivr.net/gh/ENT8R/streetcomplete-mapstyle@mapbox/streetcomplete-light-style.yaml',
-    attribution: attribution.mapbox
-});
-const mapboxDark = Tangram.leafletLayer({
-    scene: 'https://cdn.jsdelivr.net/gh/ENT8R/streetcomplete-mapstyle@mapbox/streetcomplete-dark-style.yaml',
-    attribution: attribution.mapbox
-});
-const mapboxSatellite = Tangram.leafletLayer({
-    scene: 'https://cdn.jsdelivr.net/gh/ENT8R/streetcomplete-mapstyle@master/streetcomplete-satellite-style.yaml',
-    attribution: attribution.mapbox
-});
-
-const terrestrisLight = Tangram.leafletLayer({
-    scene: 'https://cdn.jsdelivr.net/gh/ENT8R/streetcomplete-mapstyle@terrestris/streetcomplete-light-style.yaml',
-    attribution: attribution.terrestris
-});
-const terrestrisDark = Tangram.leafletLayer({
-    scene: 'https://cdn.jsdelivr.net/gh/ENT8R/streetcomplete-mapstyle@terrestris/streetcomplete-dark-style.yaml',
-    attribution: attribution.terrestris
-});
+const layers = {
+  light: Tangram.leafletLayer({
+      scene: 'https://cdn.jsdelivr.net/gh/ENT8R/streetcomplete-mapstyle@master/streetcomplete-light-style.yaml',
+      attribution: attribution.mapzen
+  }),
+  dark: Tangram.leafletLayer({
+      scene: 'https://cdn.jsdelivr.net/gh/ENT8R/streetcomplete-mapstyle@master/streetcomplete-dark-style.yaml',
+      attribution: attribution.mapzen
+  }),
+  satellite: Tangram.leafletLayer({
+      scene: 'https://cdn.jsdelivr.net/gh/ENT8R/streetcomplete-mapstyle@master/streetcomplete-satellite-style.yaml',
+      attribution: attribution.mapzen
+  })
+};
+let active = layers.light;
 
 const map = L.map('map', {
-    layers: [mapzenLight]
+    layers: [ active ],
+    center: [
+      localStorage.getItem('lat') || 40.70531887544228,
+      localStorage.getItem('lng') || -74.00976419448853
+    ],
+    zoom: localStorage.getItem('zoom') || 15,
+    zoomControl: false
 });
-L.control.layers({
-    'Light Style (Mapzen)': mapzenLight,
-    'Light Style (Mapbox)': mapboxLight,
-    'Light Style (Terrestris)': terrestrisLight,
-    'Dark Style (Mapzen)': mapzenDark,
-    'Dark Style (Mapbox)': mapboxDark,
-    'Dark Style (Terrestris)': terrestrisDark,
-    'Satellite Style (Mapbox)': mapboxSatellite
-}).addTo(map);
 
-map.setView([40.70531887544228, -74.00976419448853], 15);
+Array.from(document.querySelectorAll('#layer-selection button')).forEach(element => {
+  element.addEventListener('click', () => {
+    document.querySelector('#layer-selection button.active').classList.remove('active');
+    element.classList.add('active');
 
-if (typeof(Storage) !== 'undefined') {
-    const lat = localStorage.getItem('lat');
-    const lng = localStorage.getItem('lng');
-    const zoom = localStorage.getItem('zoom');
+    const layer = layers[element.dataset.layer];
+    map.removeLayer(active);
+    map.addLayer(layer);
+    active = layer;
+  });
+});
 
-    if (lat !== null && lng !== null && zoom !== null) {
-      map.setView([lat, lng], zoom);
-    }
-}
-
-window.onbeforeunload = function() {
-    if (typeof(Storage) !== 'undefined') {
-        localStorage.setItem('lat', map.getCenter().lat);
-        localStorage.setItem('lng', map.getCenter().lng);
-        localStorage.setItem('zoom', map.getZoom());
-    }
-};
+window.addEventListener('beforeunload', () => {
+  window.localStorage.setItem('lat', map.getCenter().lat);
+  window.localStorage.setItem('lng', map.getCenter().lng);
+  window.localStorage.setItem('zoom', map.getZoom());
+});
