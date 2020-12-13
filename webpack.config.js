9173e6af3effbaecd571ab9c3634a71d0993d35d
ENT8R <info.ent8r@gmail.com>
2020-12-13 18:23:11 +0100

    :arrow_up: Update dependencies
    Also updates tangram to the latest version (v0.21.1)
    which resolved an issue with some textures not being shown

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,8 @@
-const path = require('path');
-
 module.exports = {
   mode: 'production',
   watch: true,
   entry: './js/main.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
     filename: '[name].min.js',
     publicPath: 'dist/'
   },
